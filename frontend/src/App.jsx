dccfa214c118799800bddb81830062995c5bc27a
James Liu <jyunrl@uci.edu>
2025-04-11 23:48:03 -0700

    implementing topbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,11 @@
-import reactLogo from "@/assets/react.svg"
-import { Button, Center, HStack } from "@chakra-ui/react"
-import viteLogo from "/vite.svg"
+import TopBar from "./TopBar";
 
 function App() {
   return (
-    <Center flexDir="column" gap="8" minH="dvh">
-      <HStack>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </HStack>
-      <HStack>
-        <Button>Click me</Button>
-        <Button>Click me</Button>
-      </HStack>
-    </Center>
-  )
+    <>
+      <TopBar></TopBar>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/TopBar.jsx
@@ -0,0 +1,24 @@
+import { HStack, Card, Image, Text } from "@chakra-ui/react";
+import { Bars3BottomLeftIcon } from "@heroicons/react/16/solid";
+
+export default function TopBar() {
+  return (
+    <>
+      <Card.Root
+        marginTop={8}
+        maxWidth={"90%"}
+        maxHeight={"10vh"}
+        mx={"auto"}
+        bgColor={"#222222"}
+      >
+        <Card.Body>
+          <HStack gap={10} alignItems={"center"}>
+            <Bars3BottomLeftIcon width={22} height={22} />
+            <Text fontSize={"20px"}>ZotFitness</Text>
+          </HStack>
+        </Card.Body>
+        <Card.Footer />
+      </Card.Root>
+    </>
+  );
+}
