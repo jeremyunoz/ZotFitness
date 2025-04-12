5e9d44e74ec6f027a20f6489e01a3647af7db4fd
James Liu <jyunrl@uci.edu>
2025-04-12 14:09:48 -0700

    added page title for each page

--- frontend/src/DashBoard.jsx
+++ frontend/src/DashBoard.jsx
@@ -1,13 +1,14 @@
 import TopBar from "./TopBar";
 import SideBar from "./SideBar";
 import MetricsCard from "./MetricsCard";
+import PageTitle from "./PageTitle";
 import { Flex, Box } from "@chakra-ui/react";
 import { FaHeart, FaTemperatureHigh, FaTint, FaWater } from "react-icons/fa";
 
 export default function Dashboard() {
   const metrics = [
     { title: "Pulse", icon: FaHeart, data: "72", unit: "bpm" },
-    { title: "Spo2", icon: FaTint, data: "98", unit: "%" },
+    { title: "SPo2", icon: FaTint, data: "98", unit: "%" },
     {
       title: "Ambient Temperature",
       icon: FaTemperatureHigh,
@@ -26,24 +27,27 @@ export default function Dashboard() {
         justifyContent={"left"}
       >
         <SideBar />
-        <Flex
-          marginTop={8}
-          wrap="wrap"
-          gap={5}
-          paddingTop={5}
-          paddingLeft={"1%"}
-          alignSelf="flex-start"
-        >
-          {metrics.map((metric, index) => (
-            <Box key={index} width={{ base: "100%", md: "calc(35%)" }}>
-              <MetricsCard
-                title={metric.title}
-                icon={metric.icon}
-                data={metric.data}
-                unit={metric.unit}
-              />
-            </Box>
-          ))}
+        <Flex flexDir={"column"}>
+          <PageTitle title="Dashboard" />
+          <Flex
+            marginTop={2}
+            wrap="wrap"
+            gap={5}
+            paddingTop={5}
+            paddingLeft={"1%"}
+            alignSelf="flex-start"
+          >
+            {metrics.map((metric, index) => (
+              <Box key={index} width={{ base: "100%", md: "calc(35%)" }}>
+                <MetricsCard
+                  title={metric.title}
+                  icon={metric.icon}
+                  data={metric.data}
+                  unit={metric.unit}
+                />
+              </Box>
+            ))}
+          </Flex>
         </Flex>
       </Flex>
     </>
